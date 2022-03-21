9e41a4b02ec870eb25556ed67bd8b8e01ee43234
Gorky <gsuquinagua@hotmail.es>
2022-03-21 17:23:49 -0500

    Add dependencies and application scaffolding

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const { addAliases } = require('module-alias')
+
+/**
+ * Register aliases: you need to declare before start server
+ */
+ addAliases({ 
+    "@routes": `${__dirname}/routes/`,
+    "@controllers": `${__dirname}/controllers/`,
+    "@providers": `${__dirname}/providers/`
+ });
+
 const express = require('express');
+const routerInit = require('./routes');
 
 const app = express();
+
+routerInit(app);
+
 
 app.listen(3000, function (){
     console.log('Application running on http://localhost:3000');
